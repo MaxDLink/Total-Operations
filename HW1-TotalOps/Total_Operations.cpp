881ca6ee3119a91a81e80776df83a1a261c9b34d
MaxDLink <max.durham.link@gmail.com>
2023-02-20 14:59:45 -0600

    explanatory comments & DS_STORE to .gitignore

--- HW1-TotalOps/Total_Operations.cpp
+++ HW1-TotalOps/Total_Operations.cpp
@@ -9,9 +9,9 @@ using namespace std;
 
 int main()
 {   
-  //know a(2N/b^t) works for every level but replace t with log base 2 of N 
-  //know log base 2 of N gives tree height but want to exclude level that hits base case so S = log base 2 of N - 1 
-  //know N+N = 2N is level - 0 
+  //know log base 2 of N gives tree height but want to exclude level that hits base case so subtract 1: S = log base 2 of N - 1 
+  //know a(2N/b^t) works calculates the total operations per level but should replace t with log base 2 of N (height of tree)
+  //know N+N = 2N is level - 0 so include in formula to get level 0 operations 
 
   int N = 16; //value of N for recursive function 
   int A = 4; //value of A is number of times recursion happens 
@@ -19,6 +19,7 @@ int main()
   int S = log2(N) - 1; //value of S is log2(N) - 1 
 
   int totalOperations = S*((2*N)+A*((2*N)/pow(B,S))); 
+  //Time complexity: O(S*((2*N)+A*((2*N)/pow(B,S))))
   cout << "The total number of Operations for N of " << N << " is: " << totalOperations << endl; 
   
 
